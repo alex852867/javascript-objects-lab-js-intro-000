986c996e29967f67ce01b714848994a45205d72c
Alex East <alexeastltd@gmail.com>
2019-05-16 10:09:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key]=value
   return object
   }
+var object = {key : value}
+var newObject = Object.assign({}, object)
+
+function deleteFromObjectByKey(object, key) {
+  delete newObject.key
+  return object
+}
