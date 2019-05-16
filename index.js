81459d132830d176144dd9500642225437335d96
Alex East <alexeastltd@gmail.com>
2019-05-16 09:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function updateObjectWithKeyAndValue(object, key, value){
 object[key]=value
 return object
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, [key])
+}
